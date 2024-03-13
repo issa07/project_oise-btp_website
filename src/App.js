5124655f617e71ed40aed65230c241249c865b85
issa07 <issa9696@hotmail.fr>
2024-03-13 13:01:43 +0100

    Added responsive navbar with completed menu button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Header from './components/Header';
 import Section from './components/Section';
 import Footer from './components/Footer';
