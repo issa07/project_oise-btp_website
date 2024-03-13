9266aaa74ad3dd01d901fe12a680b63a4931faf2
issa07 <issa9696@hotmail.fr>
2024-03-13 13:03:40 +0100

    Added responsive navbar with completed menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import React, { useState } from 'react'
 
 export default function Header() {
 
-    function handleClick(event) {
-        alert("Vous avec clique sur : " + event.target.innerText);
-    }
+    // function handleClick(event) {
+    //     alert("Vous avec clique sur : " + event.target.innerText);
+    // }
 
     return (
         <div className='navbars'>
@@ -37,16 +37,16 @@ export default function Header() {
                                         {/* <div className="collapse navbar-collapse" id="navbarNav"> */}
                                             <ul className="navbar-nav">
                                                 <li className="nav-item">
-                                                    <a onClick={handleClick} className="nav-link" aria-current="page" href="contact">Contact <p className='text-smal'>Tel : 00 00 00 00 00 <br /> Gmail : sarloisebtp@gmail.com</p></a>
+                                                    <a className="nav-link" aria-current="page" href="contact">Contact <p className='text-smal'>Tel : 00 00 00 00 00 <br /> Gmail : sarloisebtp@gmail.com</p></a>
                                                 </li>
                                                 <li className="nav-item">
-                                                    <a onClick={handleClick} className="nav-link" href="our_services">Nos Services</a>
+                                                    <a className="nav-link" href="our_services">Nos Services</a>
                                                 </li>
                                                 <li className="nav-item">
-                                                    <a onClick={handleClick} className="nav-link" href="galleries">Galleries</a>
+                                                    <a className="nav-link" href="galleries">Galleries</a>
                                                 </li>
                                                 <li className="nav-item">
-                                                    <a onClick={handleClick} className="nav-link" href="about" >A propos de nous</a>
+                                                    <a className="nav-link" href="about" >A propos de nous</a>
                                                 </li>
                                             </ul>
                                         {/* </div> */}
