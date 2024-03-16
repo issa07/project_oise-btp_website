9c02be8cbe5c0f154221b552f9f4af4dd441366a
issa07 <issa9696@hotmail.fr>
2024-03-16 23:26:54 +0100

    Section Bio update && Section Nos Services update

--- src/components/SectionOurServices.js
+++ src/components/SectionOurServices.js
@@ -8,17 +8,33 @@ export default function SectionOurServices() {
         <div className="container">
             <h1 className="title-ourServices"><strong>Nos services</strong></h1>
             <div className="row">
-                <div className="col">
-                    <img src={process.env.PUBLIC_URL + '/img/ravalement-projete.jpg'} alt="" className="img-services" />
-                    <p>Construction</p>
+                <div className="col-6 col-md-4 div-service">
+                    <img src={process.env.PUBLIC_URL + '/img/construction-img.jpg'} alt="" className="img-fluid" />
+                    <h4><b> Construction </b></h4>
+                    <p>
+                        Oise BTP excelle dans la construction sur mesure de maisons, 
+                        bâtiments commerciaux et industriels. 
+                        Avec une expertise solide et un souci du détail, 
+                        nous concrétisons vos projets selon vos spécifications exactes, 
+                        garantissant des structures solides et durables.
+                    </p>
                 </div>
-                <div className="col">
-                    <img src={process.env.PUBLIC_URL + '/img/construction-img.jpg'} alt="" className="img-services" />
-                    <p>Rénovation</p>
+                <div className="col-6 col-md-4 div-service">
+                    <img src={process.env.PUBLIC_URL + '/img/renovation-img.jpeg'} alt="" className="img-fluid" />
+                    <h4><b> Rénovation  </b></h4>
+                    <p> 
+                        Confiez à Oise BTP la rénovation de votre maison, bureau ou espace commercial. 
+                        Notre équipe qualifiée personnalise chaque projet pour créer des environnements fonctionnels, 
+                        esthétiques et conformes à vos besoins.
+                    </p>
                 </div>
-                <div className="col">
-                    <img src={process.env.PUBLIC_URL + '/img/construction-img.jpg'} alt="" className="img-services" />
-                    <p>Ravalement projeté</p>
+                <div className="col-6 col-md-4 div-service last">
+                    <img src={process.env.PUBLIC_URL + '/img/ravalement-projete.jpg'} alt="" className="img-fluid" />
+                    <h4><b> Ravalement projeté  </b></h4>
+                    <p>
+                        Oise BTP offre des services de ravalement projeté de haute qualité pour rafraîchir et renforcer la durabilité de vos façades. 
+                        Avec des techniques avancées et des matériaux de première qualité, nous améliorons l'attrait et la valeur de votre propriété.
+                    </p>
                 </div>
             </div>
         </div>
