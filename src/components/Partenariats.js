13993a712896cf003e09b5e6d345d0fa6bd4b1e2
issa07 <issa9696@hotmail.fr>
2024-03-29 17:14:46 +0100

    La partie Nos partenariats a ete ajoutee

--- src/components/Partenariats.js
+++ src/components/Partenariats.js
@@ -4,13 +4,20 @@ import '../styles/partenariats.css'
 export default function Partenariats() {
     return (
         <div className="bg-partenariats">
-            <div className='container div-partenariats'>
+            <div className='container-fluid div-partenariats'>
                 <div className="row">
-                    <div className="col">
-                        <h1> Column 1 </h1>
+                    <h1> Nos Partenariats </h1>
+                    <div className="col-xs-6 col-sm-3  div-img-partenariats ">
+                        <img src={process.env.PUBLIC_URL + "/img/by_construction.png"} alt="" style={{ width: '50%' }} />
                     </div>
-                    <div className="col">
-                        <h1> Column 2 </h1>
+                    <div className="col-xs-6 col-sm-3  div-img-partenariats">
+                        <img src={process.env.PUBLIC_URL + "/img/eiffage_construction.png"} alt="" style={{ width: '50%' }}  />
+                    </div>
+                    <div className="col-xs-6 col-sm-3  div-img-partenariats">
+                        <img src={process.env.PUBLIC_URL + "/img/picard.png"} alt="" style={{ width: '50%' }}  />
+                    </div>
+                    <div className="col-xs-6 col-sm-3  div-img-partenariats">
+                        <img src={process.env.PUBLIC_URL + "/img/eiffage_construction.png"} alt="" style={{ width: '50%' }}  />
                     </div>
                 </div>
             </div>
