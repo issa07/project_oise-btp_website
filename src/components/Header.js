e32491abfac0ea35bff5cdec0b6ba383dcbfe18a
issa07 <issa9696@hotmail.fr>
2024-04-02 15:47:40 +0200

    deploy app

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,17 +8,17 @@ import '../styles/navbar.css';
 export default function Header() {
 
     const [isSmallScreen, setIsSmallScreen] = useState(false);
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
-  const handleToggle = () => {
-    setOpen(!open);
-  };
+    const handleToggle = () => {
+        setOpen(!open);
+    };
 
-  const checkScreenSize = () => {
-    setIsSmallScreen(window.innerWidth < 768);
-  };
+    const checkScreenSize = () => {
+        setIsSmallScreen(window.innerWidth < 768);
+    };
 
-  window.addEventListener('resize', checkScreenSize);
+    window.addEventListener('resize', checkScreenSize);
     
     return ( 
         <>
@@ -34,28 +34,28 @@ export default function Header() {
                                     <span className="navbar-toggler-icon"></span>
                                 </Button>
                             ) : (
-                                    <List className='offcanvas-body drawer-list '>
-                                        <ListItem button style={{alignItems: 'baseline'}}>
-                                            <ListItemText>
-                                                <a className="nav-link" aria-current="page" href="#contact">Contact <p className='text-smal'>Tel : 00 00 00 00 00 <br /> Gmail : sarloisebtp@gmail.com</p></a>
-                                            </ListItemText>
-                                        </ListItem>
-                                        <ListItem button style={{alignItems: 'baseline'}}>
-                                            <ListItemText>
-                                                <a className="nav-link" href="#about" >A propos de nous</a>
-                                            </ListItemText>
-                                        </ListItem>
-                                        <ListItem button style={{alignItems: 'baseline'}}>
-                                            <ListItemText>
-                                                <a  className="nav-link" href="#services">Nos Services</a>
-                                            </ListItemText>
-                                        </ListItem>
-                                        <ListItem button style={{alignItems: 'baseline'}}>
-                                            <ListItemText>
-                                                <a className="nav-link" href="#partenariats">Nos partenariats</a>
-                                            </ListItemText>
-                                        </ListItem>
-                                    </List>
+                                <List className='offcanvas-body drawer-list '>
+                                    <ListItem button style={{alignItems: 'baseline'}}>
+                                        <ListItemText>
+                                            <a className="nav-link" aria-current="page" href="#contact">Contact <p className='text-smal'>Tel : 00 00 00 00 00 <br /> Gmail : sarloisebtp@gmail.com</p></a>
+                                        </ListItemText>
+                                    </ListItem>
+                                    <ListItem button style={{alignItems: 'baseline'}}>
+                                        <ListItemText>
+                                            <a className="nav-link" href="#about" >A propos de nous</a>
+                                        </ListItemText>
+                                    </ListItem>
+                                    <ListItem button style={{alignItems: 'baseline'}}>
+                                        <ListItemText>
+                                            <a  className="nav-link" href="#services">Nos Services</a>
+                                        </ListItemText>
+                                    </ListItem>
+                                    <ListItem button style={{alignItems: 'baseline'}}>
+                                        <ListItemText>
+                                            <a className="nav-link" href="#partenariats">Nos partenariats</a>
+                                        </ListItemText>
+                                    </ListItem>
+                                </List>
                             )}
                             <Drawer anchor="right" open={open} onClose={handleToggle} >
                                 <List>
